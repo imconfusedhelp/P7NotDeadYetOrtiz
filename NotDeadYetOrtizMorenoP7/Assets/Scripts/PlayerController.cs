d409d6a4e1cb041aa1840faf7942b2ff1f4a5ad5
imconfusedhelp <demskillzdough721129@gmail.com>
2023-05-22 15:28:14 -0700

    Update 05/22/23 3:27pm
    FINISH FIXING ANIMATIONS
    Get started on zombie programming

--- NotDeadYetOrtizMorenoP7/Assets/Scripts/PlayerController.cs
+++ NotDeadYetOrtizMorenoP7/Assets/Scripts/PlayerController.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float speed;
 
-    private float speed = 10.0f;
+    Animator animator;
+
+    Rigidbody2D rigidbody2d;
+
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    float horizontal;
+    float vertical;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
 
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
+        Vector2 move = new Vector2(horizontal, vertical);
 
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+
+        // set speed and direction
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
