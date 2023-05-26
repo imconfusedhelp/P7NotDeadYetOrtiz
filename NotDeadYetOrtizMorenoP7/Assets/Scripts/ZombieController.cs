c9a5ad79f1e87bfd6ef067a59260ff5d97bd51fa
imconfusedhelp <demskillzdough721129@gmail.com>
2023-05-26 15:28:20 -0700

    Update 05/26/23 3;27pm
    Added zombie sprite and animations
    made a small zombie movement script that needs work
    need to create title screen and such

---
+++ NotDeadYetOrtizMorenoP7/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
